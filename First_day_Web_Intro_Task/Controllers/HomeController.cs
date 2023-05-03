9f4fdc3cbb202cee072ff9e39a07adb468a93689
BlkerGuy <2020.gafarov@gmail.com>
2023-05-04 02:54:40 +0400

    Add project files.

---
+++ First_day_Web_Intro_Task/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace First_day_Web_Intro_Task.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            ViewResult viewResult = new ViewResult();
+            viewResult.ViewName = "test";
+            return viewResult;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public string Index()
+        //{
+        //    return "Main Page";
+        //}
+
+        public JsonResult About()
+        {
+            JsonResult json = new JsonResult("About Page");
+            json.StatusCode = 200;
+            return json;
+        }
+        public ContentResult Blog()
+        {
+            ContentResult contentResult=new ContentResult();
+            contentResult.Content = "Blog Page Just Test";
+            contentResult.ContentType = "test";
+
+            return contentResult;
+        }
+
+    }
+}
